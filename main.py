1df9fa8d3ca08d2c7de7d07e8bf14e8ca679c844
hyunsookimm <khsoo0512@gmail.com>
2026-05-14 16:30:28 +0900

    .

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from controller import items,users
+
+app = FastAPI()
+
+app.include_router(items.router)
+app.include_router(users.router)
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
+
+
+
